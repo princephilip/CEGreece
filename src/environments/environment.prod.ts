c290d33f139152acfdb468ff5cd6aa3624bed300
princephilip <o.philip@cityu.gr>
2019-01-03 15:44:25 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyCnt0HoejwSW2XBEdrVwe_VkQvqSxKQt0Q",
-    authDomain: "christ-embassy-greece.firebaseapp.com",
-    databaseURL: "https://christ-embassy-greece.firebaseio.com",
-    projectId: "christ-embassy-greece",
-    storageBucket: "christ-embassy-greece.appspot.com",
-    messagingSenderId: "11459122627"
+    apiKey: "private_key",
+    authDomain: "private",
+    databaseURL: "private",
+    projectId: "private",
+    storageBucket: "private",
+    messagingSenderId: "private_id"
   }
 };
